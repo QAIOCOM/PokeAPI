788f520f99f6e819bed17fec8c8fb4517ddd408d
Imran-Subhan <isubhan@qa.com>
2020-07-24 13:49:46 +0100

    changed work

--- abilities.js
+++ abilities.js
@@ -1,33 +1,50 @@
+let userUpperLimit;
+let userLowerLimit;
 
-function getAbilities() {
-    fetch('https://pokeapi.co/api/v2/ability/')
+
+
+function sendValues(){
+    userLowerLimit = document.getElementById("userLowerLimit").value;
+    userUpperLimit = document.getElementById("userUpperLimit").value;
+    limit(userLowerLimit, userUpperLimit);
+    console.log("yo we getting intothis funtcion?");
+}
+
+function limit(userLowerLimit, userUpperLimit) {
+    document.getElementById("tbody").innerHTML="";
+    fetch('https://pokeapi.co/api/v2/ability/?offset=' + userLowerLimit + '&limit=' + (userUpperLimit-userLowerLimit))
     .then(
     function(response) {
-        if (response.status !== 200) {
-        console.log('Looks like there was a problem. Status Code: ' +
-            response.status);
-        return;
-        }
-
-        // Examine the text in the response
-        response.json().then(function(data) {
+    if (response.status !== 200) {
+    console.log('Looks like there was a problem. Status Code: ' +
+    response.status);
+    return;
+    }
+    response.json().then(function(data) {
         console.log(data);
-        for (let i = 0; i < 200; i++) {
-            console.log(
-            (document.querySelector("#resp").innerHTML = data.results[i].name)
-            );
+        for (let i = 0; i < 293; i++) {
+        console.log(data.results[i].name);
+        console.log(data.results[i].url);
+        
+        let para = document.createElement("P");
+        let tbody = document.getElementById("tbody") // Create a <p> element
+        para;
+        tbody;
+        console.log(tbody);
+        let string = '<tr><td>'+data.results[i].name+'</td><td>'+data.results[i].url+'</tr>';
+       
+        document.getElementById("tbody").innerHTML+=string;
+        // para.className = "alert alert-success col-md-8";
+        // para.innerText = `The name is : ${(data.results[i].name)} \n
+        // The url is : ${data.results[i].url}`; // Insert text
 
-            let para = document.createElement("P"); // Create a <p> element
-            para;
-            para.className = "alert alert-danger col-md-8";
-            para.innerText = `Ability Name: ${(document.querySelector("#resp").innerHTML = data.results[i].name)}`;
-
-            let myDiv = document.getElementById("todos");
-            myDiv.appendChild(para);
+      
         }
         });
-    }
-    )
+        })
+
+
+
     .catch(function(err) {
     console.log('Fetch Error :-S', err);
     });
